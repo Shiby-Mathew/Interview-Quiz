6fca7d8820cf10cbaf2ec480859029bcc9f51a5a
Shiby-Mathew <shiby.ajay@gmail.com>
2023-03-06 09:50:30 +1100

    showing highscore in html page not working

--- assets/highScore.js
+++ assets/highScore.js
@@ -1,6 +1,14 @@
+var listEl = document.querySelector("#highscores");
+
 function showScores() {
   var scoreInfo = JSON.parse(window.localStorage.getItem("scoreInfo"));
-  console.log(scoreInfo);
+  console.log(scoreInfo.initialName);
+
+  var li1 = document.createElement("li");
+  li1.textContent =
+    "Initials : " + scoreInfo.initialName + " - Score: " + scoreInfo.totalScore;
+  console.log("display");
+  listEl.appendChild(li1);
 }
 
 //store finalscore in local storage
